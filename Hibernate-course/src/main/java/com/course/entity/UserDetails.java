706581f73ab19563e0772096eb675b77604c3aa8
Evandro <evandrolmass@gmail.com>
2015-05-13 18:58:59 -0300

    Class update.

--- Hibernate-course/src/main/java/com/course/entity/UserDetails.java
+++ Hibernate-course/src/main/java/com/course/entity/UserDetails.java
@@ -5,18 +5,36 @@
  */
 package com.course.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author Evandro
  */
+//@Entity (name="USER_DETAILS") - Works too
 @Entity
+@Table (name="USER_DETAILS")
 public class UserDetails {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
+    @Column (name="USER_NAME")
     private String userName;
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    private String adress;
+    @Lob
+    private String description;
+    
 
     /**
      * @return the userId
@@ -36,7 +54,7 @@ public class UserDetails {
      * @return the userName
      */
     public String getUserName() {
-        return userName;
+        return userName+" from getter.";
     }
 
     /**
@@ -44,6 +62,48 @@ public class UserDetails {
      */
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    /**
+     * @return the joinedDate
+     */
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+
+    /**
+     * @param joinedDate the joinedDate to set
+     */
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    /**
+     * @return the adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * @param adress the adress to set
+     */
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     
--- Hibernate-course/src/main/java/com/source/test/HibernateTest.java
+++ Hibernate-course/src/main/java/com/source/test/HibernateTest.java
@@ -6,25 +6,35 @@
 package com.source.test;
 
 import com.course.entity.UserDetails;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.postgresql.jdbc2.optional.SimpleDataSource;
 
 /**
  *
  * @author Evandro
  */
 public class HibernateTest {
-    public static void main (String[] args){
+    public static void main (String[] args) throws ParseException{
         UserDetails user = new UserDetails();
-        user.setUserId(58);
         user.setUserName("Joelma");
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("2015-05-13");
+        user.setJoinedDate(date);
+        
+//        entityFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(user);
         session.getTransaction().commit();
-        
+        session.close();
     }
 }
